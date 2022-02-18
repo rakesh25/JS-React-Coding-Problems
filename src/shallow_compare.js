5bbdb9757667edf7dfd8c06bd53925dd01be5f8a
Rakesh kumar <krakesh.cse@gmail.com>
2022-02-17 18:07:59 -0800

    Fix shallow compare Arrays compare fix

--- src/shallow_compare.js
+++ src/shallow_compare.js
@@ -3,18 +3,32 @@ function isObject(obj) {
 }
 
 function shallowEqual(x, y) {
-  //Object.is() determines if x and y are same values/object (true if equal else false)
+  //Object.is() determines if x and y are same values/object/array (true if equal else false)
   //basically this would help us to evaluate primitives equality
   if (Object.is(x, y)) {
     return true;
   }
 
-  //if both are objects, then iterate over keys and compare values
+  //if both are arrays
+  if (Array.isArray(x) && Array.isArray(y)) {
+    if (x.length !== y.length) {
+      return false;
+    }
+
+    x.forEach((item, i) => {
+      if (x[i] !== y[i]) {
+        return false;
+      }
+    });
+  }
+
+  //if both are objects/arrays, then iterate over keys and compare values
   if (isObject(x) && isObject(y)) {
     //Can't use Object.keys because it only returns enumerable keys, not non-enumerable keys
 
     //Since we want all properties(not inherited ones),
     //We should use Object.getOwnPropertyNames
+    //getOwnPropertyNames returns [0,1,2,3, length] for arr [1,2,3,4] which is why we handled arrays above (we could have used keys as well)
     let propertiesX = Object.getOwnPropertyNames(x);
     let propertiesY = Object.getOwnPropertyNames(y);
 
@@ -27,8 +41,6 @@ function shallowEqual(x, y) {
         return false;
       }
     });
-
-    return true;
   }
 
   return true;
